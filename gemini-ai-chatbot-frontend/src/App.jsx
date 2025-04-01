f4395e3db9faecfd4b38edc351866b4220727194
parshu <parshuram.m@accops.com>
2025-04-01 22:05:49 +0530

    Added Chat Response page

--- gemini-ai-chatbot-frontend/src/App.jsx
+++ gemini-ai-chatbot-frontend/src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import ChatInput from "./compontents/ChatInput";
 import ChatResponse from "./compontents/ChatResponse";
 
 function App() {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleQuestionSubmit = async (question) => {
+    setLoading(true);
+    setResponse(null);
+    try {
+      //Await
+    } catch (error) {
+      alert("Failed to get Response");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
       <div className="App">
         <header className="bg-primary text-white text-center py-4">
           <h1>Gemini AI ChatBot</h1>
         </header>
-        <ChatInput />
-        <ChatResponse />
+        <ChatInput onSubmit={handleQuestionSubmit} />
+        <ChatResponse response={response} />
       </div>
     </>
   );
--- gemini-ai-chatbot-frontend/src/compontents/ChatResponse.jsx
+++ gemini-ai-chatbot-frontend/src/compontents/ChatResponse.jsx
@@ -1,5 +1,44 @@
-function ChatResponse() {
-  return <h1>Response</h1>;
+function ChatResponse({ response }) {
+  if (!response) {
+    return null;
+  }
+  const { candidates, usageMetadata } = response;
+  return (
+    <div className="container my-4">
+      <h3>Response</h3>
+      {candidates.map((candidate, index) => (
+        <div class="card mb-3" key={index}>
+          <div class="card-body">
+            <h5 class="card-title">Candidate {index + 1}</h5>
+            <p class="card-text">{candidate.content.parts[0].text}</p>
+            <h6>
+              Citations :
+              <u>
+                {candidate?.citationMetadata?.citationSources.map(
+                  (source, idx) => {
+                    <li key={idx}>
+                      <a
+                        href={source.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {source.url}
+                      </a>{" "}
+                      (Indexes:{source.startIndex}-{source.endIndex})
+                    </li>;
+                  }
+                )}
+              </u>
+            </h6>
+          </div>
+        </div>
+      ))}
+      <h4>Usage Metadata</h4>
+      <p>Prompt Tokens :{usageMetadata.promptTokenCount}</p>
+      <p>Prompt Response Tokens :{usageMetadata.candidatesTokenCount}</p>
+      <p>Total Tokens :{usageMetadata.totalTokenCount}</p>
+    </div>
+  );
 }
 
 export default ChatResponse;
