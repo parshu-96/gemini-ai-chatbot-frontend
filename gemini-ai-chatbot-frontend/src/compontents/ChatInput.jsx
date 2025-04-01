479d87a3ed895308d3d3ecc9578452fc8ef35e23
parshu <parshuram.m@accops.com>
2025-04-01 21:38:58 +0530

    Added Form Structure for ChatInput UI component

--- gemini-ai-chatbot-frontend/src/compontents/ChatInput.jsx
+++ gemini-ai-chatbot-frontend/src/compontents/ChatInput.jsx
@@ -1,5 +1,35 @@
-function ChatInput() {
-  return <h1>Input</h1>;
+import { useState } from "react";
+
+function ChatInput({ onSubmit }) {
+  const [question, setQuestion] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (question.trim()) {
+      onSubmit(question);
+      setQuestion("");
+    }
+  };
+
+  return (
+    <div className="container my-4">
+      <div onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="question">Ask a Question</label>
+          <input
+            type="text"
+            className="form-control"
+            id="question"
+            placeholder="Enter your Question"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-2">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ChatInput;
